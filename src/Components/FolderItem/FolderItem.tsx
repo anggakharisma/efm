0b7af791c4ed9c5d8dc6b00de28e554e95e6da65
Angga Kharisma <anggaputrakharisma@gmail.com>
2020-09-05 14:23:42 +0700

    add double click event on folder click

--- src/Components/FolderItem/FolderItem.tsx
+++ src/Components/FolderItem/FolderItem.tsx
@@ -1,10 +1,13 @@
 import './FolderItem.scss';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {FolderType} from '../../types';
 
 export default function FolderItem(props: FolderType) {
+    useEffect(() => {
+
+    }, []);
     return (
-        <div className="folder-item">
+        <div className="folder-item" onDoubleClick={ () => console.log("Clicked") }>
             <h1>ICON</h1>
             <h3>{ props.name }</h3>
         </div>
