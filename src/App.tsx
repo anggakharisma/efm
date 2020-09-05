dd3be5a03f9c85764559c98030fea6d9c3316b80
Angga Kharisma <anggaputrakharisma@gmail.com>
2020-09-05 22:22:27 +0700

    Fix security

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './Stylesheets/App.scss'
+import './Stylesheets/App.scss';
 
 import React from 'react';
 import Default from './Screens/Default/Default';
 
-
 function App() {
-    return <Default />
+	return <Default />
 }
 
 export default App;
