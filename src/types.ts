30430da9ca902c108b2961041e66cbd4a3fa1c56
Angga Kharisma <anggaputrakharisma@gmail.com>
2020-09-05 22:24:27 +0700

    Fix format

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,32 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
 export interface Drive {
-    enumerator: string,
-    busType: string,
-    busVersion: string,
-    device: string,
-    devicePath: string,
-    raw: string,
-    description: string,
-    error: string,
-    size: number,
-    blockSize: number,
-    logicalBlockSize: number,
-    mountpoints: { path: string }[],
-    isReadOnly: boolean,
-    isSystem: boolean,
-    isVirtual: boolean,
-    isRemovable: boolean,
-    isCard: boolean,
-    isSCSI: boolean,
-    isUSB: boolean,
-    isUAS: boolean,
+	enumerator: string
+	busType: string
+	busVersion: string
+	device: string
+	devicePath: string
+	raw: string
+	description: string
+	error: string
+	size: number
+	blockSize: number
+	logicalBlockSize: number
+	mountpoints: { path: string }[]
+	isReadOnly: boolean
+	isSystem: boolean
+	isVirtual: boolean
+	isRemovable: boolean
+	isCard: boolean
+	isSCSI: boolean
+	isUSB: boolean
+	isUAS: boolean
 }
 
 export interface FolderType {
-    name: string;
-    clickFunc: (e: any) => void;
-    hash: string;
+	name: string
+	clickFunc: (e: any) => void
+	hash: string
 }
 
 export interface ContentWrapperType {
