7f516222c3121c26e5516807801e448a376013c2
Angga Kharisma <anggaputrakharisma@gmail.com>
2020-12-19 13:24:19 +0700

    Never minda

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"arrowParens": "avoid",
+	"endOfLine": "crlf",
+	"jsxSingleQuote": true,
+	"semi": false,
+	"singleQuote": true,
+	"trailingComma": "none",
+	"useTabs": true,
+	"printWidth": 65
+}
